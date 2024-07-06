296eca5c9ada5de3a74f86bfaa7c35eb9f0dfd64
Manith Ratnayake <manithbbratnayake@gmail.com>
2024-07-06 23:25:58 +0530

    MAC changer
    Simple python script to automate MAC address changes

---
+++ MAC_changer.py
@@ -0,0 +1,30 @@
+import subprocess
+
+
+def change_mac(interface, new_mac):
+    # Bring the interface down
+    subprocess.call(f"ifconfig {interface} down", shell=True)
+    
+    # Change the MAC address
+    subprocess.call(f"ifconfig {interface} hw ether {new_mac}", shell=True)
+    
+    # Bring the interface up
+    subprocess.call(f"ifconfig {interface} up", shell=True)
+
+def main():
+    interface = input("Enter the interface name: ")
+    new_mac = input("Enter the new MAC address: ")
+
+    # Display the current MAC address
+    print("[*] Current MAC address:")
+    subprocess.call("ifconfig", shell=True)
+
+    # Change the MAC address
+    change_mac(interface, new_mac)
+
+    # Confirm the change
+    print(f"[+] MAC address has been successfully changed to {new_mac}")
+    subprocess.call("ifconfig", shell=True)
+
+if __name__ == "__main__":
+    main()
